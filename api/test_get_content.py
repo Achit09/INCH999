7e1b74f166ab78f511bc52bae6253bc8b51404bd
Achit09 <72000748+Achit09@users.noreply.github.com>
2020-09-29 14:28:58 +0800

    master

--- api/test_get_content.py
+++ api/test_get_content.py
@@ -11,3 +11,28 @@
 #  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #  License for the specific language governing permissions and limitations
 #  under the License.
+
+from __future__ import unicode_literals, absolute_import
+
+import unittest
+
+from linebot import (
+    LineBotApi
+)
+
+
+class TestLineBotApi(unittest.TestCase):
+    def setUp(self):
+        self.tested = LineBotApi('channel_secret')
+
+    def test_get_content(self):
+        pass
+        # message_content = self.tested.get_content(12345)
+        # with open('/tmp/content.jpg', 'wb') as f:
+        #     for chunk in message_content.iter_content():
+        #         f.write(chunk)
+        #     f.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
