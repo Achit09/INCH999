3546408d5689cce8b4c2b81c991172181a736e9f
Achit09 <72000748+Achit09@users.noreply.github.com>
2020-09-29 14:26:51 +0800

    master

--- __init__.py
+++ __init__.py
@@ -14,22 +14,22 @@
 
 """linebot package."""
 
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
-from .__about__ import (  # noqa
-    __version__
-)
-from .api import (  # noqa
-    LineBotApi,
-)
-from .http_client import (  # noqa
-    HttpClient,
-    RequestsHttpClient,
-    HttpResponse,
-)
-from .webhook import (  # noqa
-    SignatureValidator,
-    WebhookParser,
-    WebhookHandler,
-    WebhookPayload,
-)
+# from .__about__ import (  # noqa
+#     __version__
+# )
+# from .api import (  # noqa
+#     LineBotApi,
+# )
+# from .http_client import (  # noqa
+#     HttpClient,
+#     RequestsHttpClient,
+#     HttpResponse,
+# )
+# from .webhook import (  # noqa
+#     SignatureValidator,
+#     WebhookParser,
+#     WebhookHandler,
+#     WebhookPayload,
+# )
