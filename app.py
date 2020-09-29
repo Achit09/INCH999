9339f9a328176fbf5c3df04b91ddb12a8f5f4086
Achit09 <72000748+Achit09@users.noreply.github.com>
2020-09-29 15:49:25 +0800

    master

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    # app.logger.info("Request body: " + body)
 
     # parse webhook body
     try:
