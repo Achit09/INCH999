73ffd5ba4733f31b15840bd36252e3d07d880407
Achit09 <72000748+Achit09@users.noreply.github.com>
2020-10-07 16:53:39 +0800

    master

--- app.py
+++ app.py
@@ -168,5 +168,4 @@ def handle_msg_text(event):
     line_bot_api.reply_message(event.reply_token,message)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
